fd96fb74e743741aae099266bd69c8abed33b606
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-09 16:15:42 +0100

    FEAT struttura creata con js dopo il click sul btn

--- js/app.js
+++ js/app.js
@@ -7,5 +7,25 @@ const celleTotali = celleLato ** 2; // celleLato alla seconda che è uguale a sc
 
 console.log(celleLato, celleTotali)
 
+const playButtonElement = document.getElementById('game__button');
+console.log(playButtonElement)
+
 const grigliaElement = document.querySelector('.griglia');
 console.log(grigliaElement);
+
+playButtonElement.addEventListener ('click' , function() {
+
+    for (let i = 0; i < celleTotali; i++ ) {
+
+        let numeroIncremento = i + 1; //per far partire da 1 e arrivare a 100, senza questo passaggio partirebbe da 0 e si fermerebbe a 99
+
+        // console.log(numeroIncremento);
+        let creazioneDiv = `<div class="cella" > ${numeroIncremento} </div>`;
+        console.log(creazioneDiv)
+
+        grigliaElement.innerHTML += creazioneDiv;
+        
+
+    }
+
+})
