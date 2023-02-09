c9d0fa884c4beb6d4e6e6cc6e4f561d88327041a
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-09 15:45:46 +0100

    FEAT struttura creata con elementi dentro html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 // console.log('Test');
+
+
+const celleLato = 10;
+
+const celleTotali = celleLato ** 2; // celleLato alla seconda che è uguale a scrivere celleLato x celleLato
+
+console.log(celleLato, celleTotali)
+
+const grigliaElement = document.querySelector('.griglia');
+console.log(grigliaElement);
